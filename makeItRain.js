406a3cb35b4dba3d2ffbd2e0d9ece01677892eaa
leroi <leroi.douglas@gmail.com>
2020-04-20 12:07:58 +0100

    Update makeItRain.js
    Changed to practice the use of Objects, constructors and prototypes

--- makeItRain.js
+++ makeItRain.js
@@ -1,58 +1,116 @@
-var maxRows = 5 // 5 rows allows for no gaps as our group of droplets
-// make their way to earth
-var maxCols = 100 // affects how heavy the rainfall is
+/*
+  I wanted to explore Objects with methods, using the prototype notation instead of 
+  object literals.
+  So this version uses an array of objects
+*/
+var Colour = function(r, g, b){
+    this.red = r;
+    this.blue = b;
+    this.green = g;
+};
 
-var xPositions = [] // how far each droplet is placed horizontally
-var yPositions = [] // where vertically our
-var speed = []
-var colors = []
-var row, col
-var animationEnabled = true
+var Position = function(x, y){
+    this.x = x;
+    this.y = y;
+};
 
-var displayArrays = function () {
-  for (row = 0; row < maxRows; ++row) {
-    for (col = 0; col < maxCols; ++col) {
-      println(col + ' ' +
-            xPositions[row][col] + '  ' +
-            yPositions[row][col] + '  ' +
-            speed[row][col] + ' ' +
-            colors[row][col])
+var Size = function(width, height){
+    this.width = width;
+    this.height = height;
+};
+
+/*
+  The raindrop object(RainDrop) has the Position, Size, and Colour objects as properties; it plus the primitive type for the speed property;
+*/
+var RainDrop = function(){
+    this.position = new Position( floor(random(5,395) ), floor(random(-400,1) ) );
+    this.size = new Size( floor(random(5,11)), floor(random(5,11)) );
+    this.speed = floor(random(1,6));
+this.colour = new Colour( floor(random(256)), floor(random(256) ), floor(random(256))); 
+};
+
+/*
+  A rain drop can draw itself
+ */
+RainDrop.prototype.draw = function() {
+    ellipse(this.position.x, this.position.y,this.size.width,this.size.height);
+};
+
+/*
+ We use this object to keep track of a collection of rain drops; that is we can track multiple (maxRows) groups of rain drops. maxCols defines how many rain drops there are in each group
+*/
+var RainDrops = function(maxRows, maxCols){
+    this.maxRows = maxRows;            // how many groups
+    this.maxCols = maxCols;            // how many drops belong to each group
+    this.drops = [];                   // where all our drops live
+};
+
+
+RainDrops.prototype.generateRain = function() {
+    for(var row = 0; row < this.maxRows; ++row){
+        // this.drops[ row ] =  new RainDrop()  ; // this  code caused undefined error
+                                                  // on all row[0][col] items but the
+                                                  // program still appeared to be
+                                                  // running; but the log method for
+                                                  // RainDrops object return the undefined
+                                                  // error. 
+                                                  
+        this.drops[ row ] =  [ new RainDrop() ] ; // this line sets the [0][col] items
+                                                  // properly
+        for(var col = 1; col < this.maxCols; ++col){
+            this.drops[ row ][ col ] = new RainDrop();
+        }
     }
-  }
-}
+};
 
-for (row = 0; row < maxRows; ++row) {
-  xPositions.push([200])
-  yPositions.push([0])
-  speed.push([5])
-  colors.push([floor(random(256)), floor(random(256)), floor(random(256))])
-
-  for (col = 1; col < maxCols; ++col) {
-    xPositions[row][col] = floor(random(5, 395))
-    yPositions[row][col] = floor(random(-400, 1))
-    speed[row][col] = floor(random(1, 6))
-    colors[row][col] = [floor(random(256)), floor(random(256)), floor(random(256))]
-  }
-}
-
-// needed only for debugging: displayArrays();
-
-var j = 0
-draw = function () {
-  background(204, 247, 255)
-
-  noStroke()
-  fill(0, 200, 255)
-
-  for (row = 0; row < maxRows; ++row) {
-    for (col = 0; col < maxCols; ++col) {
-      fill(colors[row][col][0], colors[row][col][1], colors[row][col][2])
-      ellipse(xPositions[row][col], yPositions[row][col], 10, 10)
-      yPositions[row][col] += speed[row][col]
-
-      if (yPositions[row][col] >= 405) {
-        yPositions[row][col] = -5
-      }
+/*
+  Let it rain
+*/
+RainDrops.prototype.rainFall = function() {
+    if( this.debug ){  // No need to animate while checking the log...
+        return;
     }
-  }
-}
+    for(var row = 0; row < this.maxRows; ++row){
+        for(var col = 1; col < this.maxCols; ++col){
+            fill(this.drops[row][col].colour.red, 
+                 this.drops[row][col].colour.blue, 
+                 this.drops[row][col].colour.green);
+            this.drops[row][col].draw();
+            this.drops[row][col].position.y += this.drops[row][col].speed;
+            
+            if( this.drops[row][col].position.y >= 405 ){
+                this.drops[row][col].position.y = -5;
+            }
+        }
+    }
+};
+
+
+RainDrops.prototype.log = function(){
+    for(var row = 0; row < this.maxRows; ++row){
+        for(var col = 0; col < this.maxCols; ++col){
+            println(col + " " + this.maxRows + " " + this.maxCols + " " +
+            this.drops[row][col].position.x + "  " + 
+            this.drops[row][col].position.y + "  " +
+            this.drops[row][col].size.width + "  " +
+            this.drops[row][col].size.height + "  " +
+            this.drops[row][col].speed //+ "  " +
+            );
+        }
+    }
+};
+
+var maxRows = 2,     // 5 rows allows for no gaps as our group of droplets
+                     // make their way to earth
+    maxCols = 100;
+    
+var r = new RainDrops(maxRows, maxCols);
+r.generateRain();
+//r.log();
+
+var draw = function() {
+    background(204, 247, 255);
+    r.rainFall();
+};
+
+
